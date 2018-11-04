bbe5bb626d299bcc60bf8bb5b96068ea1d695208
Anthony Nandaa <profnandaa@gmail.com>
2018-11-04 13:48:11 +0000

    feat: v1 of the CLI
    - can generate auth URL
    - can generate access and refresh tokens

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # ads-token-gen
 An NPM module for generating OAuth2 credentails for Google Adword API
 
-Start by generating the Authentication URL by running:
-
-```bash
-$ node index.js --client_id <client-id> --client_secret <client-secret> --gen_url
-```
-
-After concent, copy the code generated and use it with the command below:
-
-```bash
-$ node index.js --code <code>
-```
-
 ## How to get `client_d` and `client_secret`
 
 - Follow the [instructions here](https://developers.google.com/adwords/api/docs/guides/authentication)
 
+## Installation
+
+```bash
+$ npm install -g ads-token-gen
+```
+
+Or you can install as a _dev-dependency_ and use `npx` instead.
+
 ## Usage
 
+Start by generating the Authentication URL by running:
+
+```bash
+$ export AW_CLIENT_ID=client_id
+$ export AW_CLIENT_SECRET=clent_secret
+```
+
+```bash
+$ ads-token-gen --gen_url
+```
+
+The `auth url` will be generated and copied to the clipboard. Paste it on your browser.
+
+Follow the authorization directions and copy the code generated (at the final stage). Use the code obtained in the command below:
+
+```bash
+$ ads-token-gen --code <code>
+```
--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 #! /usr/bin/env node
-const chalk = require('chalk')
 const beautify = require('json-beautify')
+const chalk = require('chalk')
+const clipboardy = require('clipboardy')
 
 const AdsAuth = require('./lib/ads-auth')
 
 const argv = require('yargs-parser')(process.argv.slice(2))
 const log = console.log
 
-if (argv.gen_url && (!argv.client_id || !argv.client_secret)) {
-  console.error('--client_id and --client_secret must be set')
+const AW_CLIENT_ID = process.env.AW_CLIENT_ID
+const AW_CLIENT_SECRET = process.env.AW_CLIENT_SECRET
+
+if (!AW_CLIENT_ID || !AW_CLIENT_SECRET) {
+  log(chalk.red('You must export AW_CLIENT_ID and AW_CLIENT_SECRET first'))
   process.exit()
 }
 
 const auth = new AdsAuth({
-  client_id: argv.client_id,
-  client_secret: argv.client_secret
+  client_id: process.env.AW_CLIENT_ID,
+  client_secret: process.env.AW_CLIENT_SECRET,
 })
 
 // generate auth url
 if (argv.gen_url) {
   const authUrl = auth.generateAuthUrl()
-  log(authUrl)
+  clipboardy.writeSync(authUrl)
+  log(`${chalk.green('✓')} The auth URL has been ${chalk.bold.green('generated and stored in the clipboard')}. Paste it in your browser`)
 }
 
 if (argv.code) {
   auth.getToken(argv.code, (error, tokens) => {
     if (tokens) {
-      log(chalk.green('You can copy and add to a .json file'))
-      log(beautify(tokens, null, 2, 80))
+      const str = beautify(tokens, null, 2, 80)
+      clipboardy.writeSync(str)
+      log(chalk.green('Already copied in the clipboard, you can add to a .json file'))
+      log(str)
     } else {
       log(chalk.yellow('No token generated, generate the auth URL again!'))
     }
